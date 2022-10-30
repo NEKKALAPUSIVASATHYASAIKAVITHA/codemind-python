78a40e7dd793ce5e773003d73d82a68db4274d28
NEKKALAPUSIVASATHYASAIKAVITHA <83871960+NEKKALAPUSIVASATHYASAIKAVITHA@users.noreply.github.com>
2022-10-30 10:53:34 +0530

    n=int(input()) k=list(map(int,input().split())) x=0 for i in range(n-2):     if k[i]%2!=0 and k[i+1]%2==0 and k[i+2]%2!=0:         x+=1 print(x)

--- count_even_numbers_between_odd_numbers.py
+++ count_even_numbers_between_odd_numbers.py
@@ -2,6 +2,6 @@ n=int(input())
 k=list(map(int,input().split()))
 x=0
 for i in range(n-2):
-    if k[i]%2!=0 and k[i+1]%2==0 and k[i+2]!=0:
+    if k[i]%2!=0 and k[i+1]%2==0 and k[i+2]%2!=0:
         x+=1
 print(x)
